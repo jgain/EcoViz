7d9c2083744098226eadd36fc9563ff43ec88305
Adrien Peytavie <adrien.peytavie@liris.cnrs.fr>
2025-10-22 22:02:50 +0200

    Update scene texture url

--- ecoviz/viz/scene.cpp
+++ ecoviz/viz/scene.cpp
@@ -1486,7 +1486,7 @@ void Scene::exportTerrainJSON(const string terrainURL, const string terrainName,
   jsonFile << "\t\t\t\t\"Type\":   \"Blended\",\n";
   jsonFile << "\t\t\t\t\"Layers\" : [\n";
   jsonFile << "\t\t\t\t\t{\n";
-  jsonFile << "\t\t\t\t\t\t\"ColorMap\":   \"Textures/aerial_grass_rock_diff_4k.png\",\n";
+  jsonFile << "\t\t\t\t\t\t\"ColorMap\":   \"Textures/aerial_grass_rock_diff_4k.jpg\",\n";
   jsonFile << "\t\t\t\t\t\t\"NormalMap\" : \"Textures/aerial_grass_rock_nor_gl_4k.exr\",\n";
 	double sizeGrass = 30.0; // in meters
   jsonFile << "\t\t\t\t\t\t\"UVScale\" : "<<  max(tx,ty)/sizeGrass <<"\n"; //,\n";
